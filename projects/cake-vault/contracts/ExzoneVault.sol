ce99f865e0a394ad9a31c56f02b39483bfea32a4
SunburstMeoga <1025113986@qq.com>
2024-09-19 20:11:21 +0800

    feat

--- projects/cake-vault/contracts/ExzoneVault.sol
+++ projects/cake-vault/contracts/ExzoneVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
+pragma solidity 0..12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "./interfaces/IMasterChef.sol";
 
-contract CakeVault is Ownable, Pausable {
+contract ExzoneVault is Ownable, Pausable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
